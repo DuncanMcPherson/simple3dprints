e9ecb586b8ac4cfe8afd39ffb70421ca4af7ad80
Duncan McPherson <duncan.mcpherson.jobs@gmail.com>
2023-08-18 23:04:29 -0600

    initial

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  recaptchaKey: '6LcHDbonAAAAABfpR7ek0HCsFeo7cHqooRfFwxyR',
+  firebase: {
+    apiKey: "AIzaSyD-m1j--Pw8rrJIYt86E1tkdtJB-X3Q6Yw",
+    authDomain: "simple-3d-prints.firebaseapp.com",
+    databaseURL: "https://simple-3d-prints-default-rtdb.firebaseio.com",
+    projectId: "simple-3d-prints",
+    storageBucket: "simple-3d-prints.appspot.com",
+    messagingSenderId: "683862284892",
+    appId: "1:683862284892:web:d25eb052ca78ac7798e499",
+    measurementId: "G-G654MX62HZ"
+  }
+}
